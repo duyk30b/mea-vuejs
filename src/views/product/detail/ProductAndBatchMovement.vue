789e5ae5aad8577a99a45b5805b20ad5a5115f63
duyk30b <duyk30b@gmail.com>
2025-01-05 18:48:16 +0700

    update ProductAndBatchMovement.vue

--- src/views/product/detail/ProductAndBatchMovement.vue
+++ src/views/product/detail/ProductAndBatchMovement.vue
@@ -173,7 +173,7 @@ const openBlankReceiptDetail = async (receiptId: number) => {
               { value: 0, text: 'Tất cả' },
               ...batchList.map((i) => ({
                 value: i.id,
-                text: `Lô ${timeToText(i.expiryDate)} - (${i.quantity} ${product.unitBasicName})`,
+                text: `Lô ${i.id} - ${timeToText(i.expiryDate)} - (${i.quantity} ${product.unitBasicName})`,
               })),
             ]"
             :maxHeight="260"
